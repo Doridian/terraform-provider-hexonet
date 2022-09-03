63aeb9aee07eff1902d5a751710f7c2c6dd93b07
Doridian <git@doridian.net>
2022-09-03 11:56:13 -0700

    Initial commit

---
+++ hexonet/provider.go
@@ -0,0 +1,102 @@
+package hexonet
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hexonet/go-sdk/v3/apiclient"
+	"github.com/hexonet/go-sdk/v3/response"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"highperformance": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("HEXONET_HIGHPERFORMANCE", false),
+			},
+			"username": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("HEXONET_USERNAME", nil),
+			},
+			"role": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("HEXONET_ROLE", nil),
+			},
+			"password": {
+				Type:        schema.TypeString,
+				Sensitive:   true,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("HEXONET_PASSWORD", nil),
+			},
+			"mfatoken": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Sensitive:   true,
+				DefaultFunc: schema.EnvDefaultFunc("HEXONET_MFATOKEN", nil),
+			},
+			"live": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("HEXONET_LIVE", false),
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{
+			"hexonet_domain": resourceDomain(),
+		},
+		DataSourcesMap:       map[string]*schema.Resource{},
+		ConfigureContextFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	username := d.Get("username").(string)
+	password := d.Get("password").(string)
+	role := d.Get("role").(string)
+	mfatoken := d.Get("mfatoken").(string)
+	highperformance := d.Get("highperformance").(bool)
+	live := d.Get("live").(bool)
+
+	var diags diag.Diagnostics
+
+	c := apiclient.NewAPIClient()
+	if live {
+		c.UseLIVESystem()
+	} else {
+		c.UseOTESystem()
+	}
+
+	if highperformance {
+		c.UseHighPerformanceConnectionSetup()
+	} else {
+		c.UseDefaultConnectionSetup()
+	}
+
+	if role != "" {
+		c.SetRoleCredentials(username, role, password)
+	} else {
+		c.SetCredentials(username, password)
+	}
+
+	var res *response.Response
+	if mfatoken != "" {
+		res = c.Login()
+	} else {
+		res = c.Login(mfatoken)
+	}
+
+	if res.IsError() {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Unable to log into Hexonet API",
+			Detail:   res.Raw,
+		})
+		return nil, diags
+	}
+
+	return c, diags
+}
---
+++ hexonet/resource_domain.go
@@ -0,0 +1,147 @@
+package hexonet
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hexonet/go-sdk/v3/apiclient"
+	"github.com/hexonet/go-sdk/v3/response"
+)
+
+const MAX_NAMESERVERS = 12
+
+func resourceDomain() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceDomainCreate,
+		ReadContext:   resourceDomainRead,
+		UpdateContext: resourceDomainUpdate,
+		DeleteContext: resourceDomainDelete,
+		Schema: map[string]*schema.Schema{
+			"domain": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"name_servers": {
+				Type:     schema.TypeList,
+				Required: true,
+				MinItems: 1,
+				MaxItems: MAX_NAMESERVERS,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+	}
+}
+
+func makeDomainCommand(cl *apiclient.APIClient, cmd string, addData bool, d *schema.ResourceData) *response.Response {
+	domain := d.Get("domain").(string)
+	if domain == "" {
+		domain = d.Id()
+	}
+
+	req := map[string]interface{}{
+		"COMMAND": cmd,
+		"DOMAIN":  domain,
+	}
+
+	d.SetId(domain)
+
+	if addData {
+		nameserverIdx := 0
+		nameservers := d.Get("name_servers").([]interface{})
+		for _, ns := range nameservers {
+			req[fmt.Sprintf("NAMESERVER%d", nameserverIdx)] = ns.(string)
+			nameserverIdx++
+		}
+
+		for nameserverIdx < MAX_NAMESERVERS {
+			req[fmt.Sprintf("NAMESERVER%d", nameserverIdx)] = ""
+			nameserverIdx++
+		}
+	}
+
+	return cl.Request(req)
+}
+
+func handlePossibleErrorResponse(resp *response.Response) *diag.Diagnostic {
+	if !resp.IsError() {
+		return nil
+	}
+
+	return &diag.Diagnostic{
+		Severity: diag.Error,
+		Summary:  fmt.Sprintf("Error in %s", resp.GetCommandPlain()),
+		Detail:   resp.Raw,
+	}
+}
+
+func resourceDomainCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	cl := m.(*apiclient.APIClient)
+
+	var diags diag.Diagnostics
+
+	resp := makeDomainCommand(cl, "AddDomain", true, d)
+	respDiag := handlePossibleErrorResponse(resp)
+	if respDiag != nil {
+		diags = append(diags, *respDiag)
+		return diags
+	}
+
+	return diags
+}
+
+func resourceDomainRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	cl := m.(*apiclient.APIClient)
+
+	var diags diag.Diagnostics
+
+	resp := makeDomainCommand(cl, "StatusDomain", false, d)
+	respDiag := handlePossibleErrorResponse(resp)
+	if respDiag != nil {
+		diags = append(diags, *respDiag)
+		return diags
+	}
+
+	nameservers := resp.GetColumn("NAMESERVER").GetData()
+
+	d.Set("domain", d.Id())
+	d.Set("name_servers", nameservers)
+
+	return diags
+}
+
+func resourceDomainUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	cl := m.(*apiclient.APIClient)
+
+	var diags diag.Diagnostics
+
+	resp := makeDomainCommand(cl, "ModifyDomain", true, d)
+	respDiag := handlePossibleErrorResponse(resp)
+	if respDiag != nil {
+		diags = append(diags, *respDiag)
+		return diags
+	}
+
+	return resourceDomainRead(ctx, d, m)
+}
+
+func resourceDomainDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	cl := m.(*apiclient.APIClient)
+
+	var diags diag.Diagnostics
+
+	resp := makeDomainCommand(cl, "DeleteDomain", false, d)
+	respDiag := handlePossibleErrorResponse(resp)
+	if respDiag != nil {
+		diags = append(diags, *respDiag)
+		return diags
+	}
+
+	return diags
+}
